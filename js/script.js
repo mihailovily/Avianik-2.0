63a32002a808b09f64f72b3c8e841f520dc97c81
Ilya <mihailovily@tuta.io>
2024-12-23 18:46:44 +0300

    big update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
-/*$(document).ready(function () {
-    $(".menu").hide();
-    $(".hamburger").click(function () {
-        $(".menu").slideToggle("slow", function () {
-            $("#burger").addClass("cross");
-            $("#burger").removeClass("hamburger");
-        });
-    });
-
-    $(".cross").click(function () {
-        $(".menu").slideToggle("slow", function () {
-            $("#burger").addClass("hamburger");
-            $("#burger").removeClass("cross");
-        });
-    });
-
-});
-*/
-$(document).ready(function () {
-    $("#burger").click(function () {
+$(document).ready(function() {
+    $("#burger").click(function() {
         $(".menu").toggle(1000);
     });
+    $("body").on('click', '[href*="#"]', function(e) {
+        var fixed_offset = 100;
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+        e.preventDefault();
+    });
+    $("#1").click(function() {
+        $("#11").toggle(1000);
+    });
+    $("#2").click(function() {
+        $("#22").toggle(1000);
+    });
+    $("#3").click(function() {
+        $("#33").toggle(1000);
+    });
+    $('.single-item').slick();
 });
